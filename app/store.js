4050194b1df7f1a2d7dc2986b5a8517079b659d5
Rafi <mohaymen@Rafis-MacBook-Pro.local>
2022-04-01 11:15:34 +0600

    cart persist added

--- app/store.js
+++ app/store.js
@@ -1,8 +1,33 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import {
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import cartReducer from '../slices/cartSlice';
 
+const reducers = combineReducers({
+  cart: cartReducer,
+});
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducers = persistReducer(persistConfig, reducers);
+
 export const store = configureStore({
-  reducer: {
-    cart: cartReducer,
-  },
+  reducer: persistedReducers,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,20 @@
 import '../styles/globals.css';
 import { SessionProvider } from 'next-auth/react';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
 import { store } from '../app/store';
+
+const persistor = persistStore(store);
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <Provider store={store}>
-      <SessionProvider session={session}>
-        <Component {...pageProps} />
-      </SessionProvider>
+      <PersistGate loading={null} persistor={persistor}>
+        <SessionProvider session={session}>
+          <Component {...pageProps} />
+        </SessionProvider>
+      </PersistGate>
     </Provider>
   );
 }
